2be282c0bc5305cbb959486a9e57ae82783d0a72
ReneRugaba <rugabarj@gmail.com>
2022-03-22 10:40:29 +0100

    deploy16

--- database/seeders/MainSeeder/Index.ts
+++ database/seeders/MainSeeder/Index.ts
@@ -1,13 +1,11 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import Application from '@ioc:Adonis/Core/Application'
+
 
 
 export default class IndexSeeder extends BaseSeeder {
   private async runSeeder(seeder: { default: typeof BaseSeeder }) {
    
-    if (seeder.default.developmentOnly && !Application.inDev) {
-      return
-    }
+    
 
     await new seeder.default(this.client).run()
   }
